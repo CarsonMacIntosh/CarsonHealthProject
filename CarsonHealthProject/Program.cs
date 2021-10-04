e145d83ebd0483c1691338e1b13b390b893b022b
TechnoCookiethePerson <66851745+CarsonMacIntosh@users.noreply.github.com>
2021-10-04 13:33:22 -0300

    added shit

--- CarsonHealthProject/Program.cs
+++ CarsonHealthProject/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarsonHealthProject
+namespace CarsonHealthProject //no more fun and games, time to be completely fucking miserable now
 {
     class Program
     {
+        int weapon = 0;
+        int health = 100;
+        int changeWeapon = 1;
+        int damage = 0;
+        int heal = 10;
+        bool alive = true;
+
+
+        //  Weapons (for weapon and changeWeapon)
+        //  0 = Gun
+        //  1 = Bigger Gun
+        //  2 = A Rock
+        //  3 = Big Sephiroth Sword
+        //  4 = Pet Chicken
+        //  5 = The Simpsons Season 4 on DVD
+        //  6 = Nuke
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Carson Health Project");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey(true);
+            Console.Clear();
+
+
+            Console.ReadKey(true);
+
         }
+
+        string weaponName;
+
+        weaponName = weaponName;
     }
 }
