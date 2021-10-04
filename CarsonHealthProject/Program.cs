2fd330f7e6a11dee3040c7da868f1f6c8fdb4259
TechnoCookiethePerson <66851745+CarsonMacIntosh@users.noreply.github.com>
2021-10-04 14:03:47 -0300

    Desiring the kid next to me to stfu

--- CarsonHealthProject/Program.cs
+++ CarsonHealthProject/Program.cs
@@ -4,42 +4,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarsonHealthProject //no more fun and games, time to be completely fucking miserable now
+namespace CarsonHealthProject
 {
     class Program
     {
-        int weapon = 0;
-        int health = 100;
+        static int weapon = 0;
+        static int health = 100;
         int changeWeapon = 1;
         int damage = 0;
-        int heal = 10;
-        bool alive = true;
-
-
-        //  Weapons (for weapon and changeWeapon)
-        //  0 = Gun
-        //  1 = Bigger Gun
-        //  2 = A Rock
-        //  3 = Big Sephiroth Sword
-        //  4 = Pet Chicken
-        //  5 = The Simpsons Season 4 on DVD
-        //  6 = Nuke
-
-
+        int heal = 0;
+        static bool alive = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Carson Health Project");
+            // strings
+            string weaponName;
+
+            if (weapon == 0)
+            {
+                weaponName = "Gun";
+            }
+
+            if (weapon == 1)
+            {
+                weaponName = "Bigger Gun";
+            }
+
+            if (weapon == 2)
+            {
+                weaponName = "Biggest Gun";
+            }
+
+            if (weapon == 3)
+            {
+                weaponName = "Big Sephiroth Sword";
+            }
+
+            if (weapon == 4)
+            {
+                weaponName = "Love-Colored Master Spark";
+            }
+
+            if (weapon == 5)
+            {
+                weaponName = "The Simpsons Season 4 on DVD";
+            }
+
+            if (weapon == 6)
+            {
+                weaponName = "Nuke";
+            }
+
+
+            string conditionName;
+
+            if (health >= 100)
+            {
+                conditionName = "Stayin' Alive";
+            }
+
+            if (health <= 99)
+            {
+                conditionName = "Healthy";
+            }
+
+            if (health <= 75)
+            {
+                conditionName = "Not as Healthy";
+            }
+
+            if (health <= 50)
+            {
+                conditionName = "In Pain";
+            }
+
+            if (health <= 10)
+            {
+                conditionName = "Dying";
+            }
+
+            if (health <= 0)
+            {
+                conditionName = "ded. not big soup rice";
+                alive = false;
+            }
+
+            // the actual game
+
+            Console.WriteLine("Carson Conditionals Project");
             Console.WriteLine("Press any key to continue");
             Console.ReadKey(true);
             Console.Clear();
 
-
+            ShowHUD();
+            Console.WriteLine("Status: " + conditionName);
             Console.ReadKey(true);
 
         }
+        static void ShowHUD()
+        {
+            Console.WriteLine("Health: " + health);
+        }
 
-        string weaponName;
-
-        weaponName = weaponName;
     }
 }
